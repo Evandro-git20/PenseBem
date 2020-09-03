9b526f51de766ddc97b3db36bc066e34d51ab3a9
EVANDRO <67401043+Evandro-git20@users.noreply.github.com>
2020-09-03 13:43:15 -0300

    Create pensebem.py

---
+++ pensebem.py
@@ -0,0 +1,14 @@
+print()
+print('QUESTÃO 1')
+print(' É sempre um        recebê-lo em minha casa!')
+print('[a] praser '
+      '[b] prazer '
+      '[c] prasê ')
+print()
+respostas = input('Resposta:')
+if respostas in 'b':
+    print('Acertou!')
+elif respostas in 'a' 'c':
+    print('Errou!')
+else:
+    print('Ops!!! Opção Inválida!')
